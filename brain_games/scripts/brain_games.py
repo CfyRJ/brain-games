ae688932a07024043a5b8dd1a727b4d048d93fd7
Alex Kositskiy <koso-96@mail.ru>
2023-04-06 13:57:02 +0300

    update brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
 
+from brain_games.cli import welcome_user
+
+
 def Welcome():
     print('Welcome to the Brain Games!')
 
 
 def main():
     Welcome()
+    welcome_user()
 
 
 if __name__ == '__main__':
