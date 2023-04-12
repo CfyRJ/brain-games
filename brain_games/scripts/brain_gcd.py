bf7878625fa8e9eeaa98163f24f834ea43192021
Alex Kositskiy <koso-96@mail.ru>
2023-04-12 22:48:02 +0300

    add brain_gcd.py

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+from brain_games.games.brain_gcd_logic import brain_gcd
+from brain_games.scripts import brain_game_run
+
+
+def main():
+    brain_game_run.main('brain-gcd')  # for check hexlet
+    # brain_gcd() # work standart
+
+
+if __name__ == '__main__':
+    main()
