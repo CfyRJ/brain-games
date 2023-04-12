a1e728e7a635cbe39119e8222d87b19bee6a4ade
Alex Kositskiy <koso-96@mail.ru>
2023-04-12 21:16:17 +0300

    add games/brain_even_logic.py

---
+++ brain_games/games/brain_even_logic.py
@@ -0,0 +1,30 @@
+from random import randint
+
+
+def check_even(num):
+    '''
+    Checking a number for evenness.
+    :param num: int
+    :return: str
+    '''
+    res = 'no'
+
+    if num % 2 == 0:
+        res = 'yes'
+
+    return res
+
+
+def brain_even():
+    '''
+    It's a game. It asks if the number is even.
+    Waiting for a 'yes' or 'no' answer.
+    return: str
+    '''
+    num_quest = randint(0, 1000)
+    answer_game = check_even(num_quest)
+
+    print(f'Question: {num_quest}')
+    answer_user = input(f'Your answer: ')
+
+    return answer_game, answer_user
