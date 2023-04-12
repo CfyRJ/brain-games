ab6d8ad838968a04d509df52ebd47057ee920a72
Alex Kositskiy <koso-96@mail.ru>
2023-04-12 22:46:26 +0300

    add brain_gcd_logic.py

---
+++ brain_games/games/brain_gcd_logic.py
@@ -0,0 +1,34 @@
+from random import randint
+
+
+def alg_evklid(num1, num2):
+    '''
+    Implementation of the Euclid algorithm.
+    :param num1: int
+    :param num2: int
+    :return: int
+    '''
+    while num1 != 0 and num2 != 0:
+        if num1 > num2:
+            num1 = num1 % num2
+        else:
+            num2 = num2 % num1
+
+    return num1 + num2
+
+
+def brain_gcd():
+    '''
+    Calculates the greatest common multiple.
+    :return: tuple from int
+    '''
+    num1 = randint(1, 1000)
+    num2 = randint(1, 1000)
+
+    print('Find the greatest common divisor of given numbers.')
+    print(f'Question: {num1} {num2}')
+    answer_user = int(input('Your answer: '))
+
+    answer_game = alg_evklid(num1, num2)
+
+    return answer_game, answer_user
