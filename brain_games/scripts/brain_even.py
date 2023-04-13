d86bf5a818187efe27c318cbadd3aeacf0155a43
Alex Kositskiy <koso-96@mail.ru>
2023-04-13 23:02:18 +0300

    bug fix brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 
 
-from brain_games.games.brain_even_logic import brain_even
 from brain_games.scripts import brain_game_run
 
 
 def main():
-    brain_game_run.main('brain-even') #for check hexlet
-    # brain_even() # work standart
+    brain_game_run.main('brain-even')
 
 
 if __name__ == '__main__':
