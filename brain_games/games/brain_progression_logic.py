7ee0fd1a20dfa8f5a3ee8b678068dc449c8d769d
Alex Kositskiy <koso-96@mail.ru>
2023-04-13 21:13:02 +0300

    add brain_progression_logic.py

---
+++ brain_games/games/brain_progression_logic.py
@@ -0,0 +1,21 @@
+from random import randint
+
+
+def brain_progression():
+    count_el = randint(5, 10)
+    first_el = randint(1, 100)
+    step = randint(2, 17)
+    last_el = first_el + (count_el - 1) * step
+
+    progression = list(range(first_el, last_el+1, step))
+
+    missing_el_index = randint(0, count_el)
+
+    answer_game = progression[missing_el_index]
+    progression[missing_el_index] = '..'
+
+    print('What number i missing in the progression?')
+    print(f'Question: {progression}')
+    answer_user = int(input('Yuor answer: '))
+
+    return answer_game, answer_user
