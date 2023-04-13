af9edf6410120afc26a7afa291410b05ff9e819c
Alex Kositskiy <koso-96@mail.ru>
2023-04-13 22:59:06 +0300

    bug fix brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 
 
-from brain_games.games.brain_calc_logic import brain_calc
 from brain_games.scripts import brain_game_run
 
 
 def main():
-    brain_game_run.main('brain-calc')  # for check hexlet
-    # brain_calc() # work standart
+    brain_game_run.main('brain-calc')
 
 
 if __name__ == '__main__':
