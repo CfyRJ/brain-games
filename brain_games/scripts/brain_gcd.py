69a6f5d4e5e12b4a118fe752157968879acafad8
Alex Kositskiy <koso-96@mail.ru>
2023-04-13 23:10:46 +0300

    bug fix brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 
 
-from brain_games.games.brain_gcd_logic import brain_gcd
 from brain_games.scripts import brain_game_run
 
 
 def main():
-    brain_game_run.main('brain-gcd')  # for check hexlet
-    # brain_gcd() # work standart
+    brain_game_run.main('brain-gcd')
 
 
 if __name__ == '__main__':
