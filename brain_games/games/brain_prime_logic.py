9dc893955af5f5e8ff6c904383d4c4f9e170153a
Alex Kositskiy <koso-96@mail.ru>
2023-04-13 22:38:25 +0300

    add brain_prime_logic.py

---
+++ brain_games/games/brain_prime_logic.py
@@ -0,0 +1,33 @@
+from random import randint
+
+
+def is_prime(num):
+    '''
+    Is the number prime.
+    :param num: int
+    :return: str
+    '''
+    denominator = 2
+    res = 'yes'
+
+    while denominator <= num ** 0.5:
+        if num % denominator == 0:
+            res = 'no'
+            break
+
+        denominator += 1
+
+    return res
+
+
+def brain_prime():
+    quest_text = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+    quest_num = randint(2, 1000)
+
+    answer_game = is_prime(quest_num)
+
+    print(quest_text)
+    print(f'Question: {quest_num}')
+    answer_user = input('Your answer: ')
+
+    return (answer_game, answer_user)
