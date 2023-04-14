069b82e04fc4e7b0a84c9a259a0af4f94b23b819
Alex Kositskiy <koso-96@mail.ru>
2023-04-14 16:58:38 +0300

    bug fix brain_gcd_logic.py

--- brain_games/games/brain_gcd_logic.py
+++ brain_games/games/brain_gcd_logic.py
@@ -22,10 +22,11 @@ def brain_gcd():
     Calculates the greatest common multiple.
     :return: tuple from int
     '''
+    TEXT_QUEST = 'Find the greatest common divisor of given numbers.'
     num1 = randint(1, 1000)
     num2 = randint(1, 1000)
 
-    print('Find the greatest common divisor of given numbers.')
+    print(TEXT_QUEST)
     print(f'Question: {num1} {num2}')
     answer_user = int(input('Your answer: '))
 
