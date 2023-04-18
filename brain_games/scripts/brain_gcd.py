0388d3debeb53eb81f5d516b71b67340fea539e2
Alex Kositskiy <koso-96@mail.ru>
2023-04-18 21:27:32 +0300

    update brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 
 
-from brain_games.scripts import brain_game_run
+from brain_games.scripts.brain_games import Welcome
+from brain_games.cli import welcome_user
+from brain_games.games.brain_game_logic import brain_game
+from brain_games.games.brain_gcd_logic import brain_gcd
 
 
 def main():
-    brain_game_run.main('brain-gcd')
+    Welcome()
+    name = welcome_user()
+
+    if brain_game(brain_gcd):
+        print(f'Congratulations, {name}!')
+    else:
+        print(f"Let's try again, {name}!")
 
 
 if __name__ == '__main__':
