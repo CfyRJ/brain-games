834e63faca9e17b19a62cc421add04508bc0bdcb
Alex Kositskiy <koso-96@mail.ru>
2023-04-18 21:27:56 +0300

    update brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 
 
-from brain_games.scripts import brain_game_run
+from brain_games.scripts.brain_games import Welcome
+from brain_games.cli import welcome_user
+from brain_games.games.brain_game_logic import brain_game
+from brain_games.games.brain_prime_logic import brain_prime
 
 
 def main():
-    brain_game_run.main('brain-prime')
+    Welcome()
+    name = welcome_user()
+
+    if brain_game(brain_prime):
+        print(f'Congratulations, {name}!')
+    else:
+        print(f"Let's try again, {name}!")
 
 
 if __name__ == '__main__':
