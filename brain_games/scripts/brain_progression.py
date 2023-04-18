a3903826190f85e2a1bd4a3373c4e5f8560de08c
Alex Kositskiy <koso-96@mail.ru>
2023-04-18 21:28:14 +0300

    update brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 
 
-from brain_games.scripts import brain_game_run
+from brain_games.scripts.brain_games import Welcome
+from brain_games.cli import welcome_user
+from brain_games.games.brain_game_logic import brain_game
+from brain_games.games.brain_progression_logic import brain_progression
 
 
 def main():
-    brain_game_run.main('brain-progression')
+    Welcome()
+    name = welcome_user()
+
+    if brain_game(brain_progression):
+        print(f'Congratulations, {name}!')
+    else:
+        print(f"Let's try again, {name}!")
 
 
 if __name__ == '__main__':
