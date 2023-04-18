6bd1d498a62adf374fa2277450b0067f136635fb
Alex Kositskiy <koso-96@mail.ru>
2023-04-18 21:26:56 +0300

    update brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 
 
-from brain_games.scripts import brain_game_run
+from brain_games.scripts.brain_games import Welcome
+from brain_games.cli import welcome_user
+from brain_games.games.brain_game_logic import brain_game
+from brain_games.games.brain_calc_logic import brain_calc
 
 
 def main():
-    brain_game_run.main('brain-calc')
+    Welcome()
+    name = welcome_user()
+
+    if brain_game(brain_calc):
+        print(f'Congratulations, {name}!')
+    else:
+        print(f"Let's try again, {name}!")
 
 
 if __name__ == '__main__':
