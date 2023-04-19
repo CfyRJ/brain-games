a900944731bbf7520096702a39f228f2854580f7
Alex Kositskiy <koso-96@mail.ru>
2023-04-19 21:08:06 +0300

    update brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
 
-from brain_games.scripts.brain_games import Welcome
-from brain_games.cli import welcome_user
+from brain_games.scripts.brain_games import welcome
+from brain_games.cli import ask_name_user
 from brain_games.games.brain_game_logic import brain_game
 from brain_games.games.brain_game_logic import end_game
 from brain_games.games.brain_even_logic import brain_even
 
 
 def main():
-    Welcome()
-    name = welcome_user()
+    welcome()
+    name = ask_name_user()
 
     win_lose = brain_game(brain_even)
 
