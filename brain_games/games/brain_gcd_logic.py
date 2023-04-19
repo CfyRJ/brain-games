3434e0b65f2c8b0f9b408a14f17071425dae1416
Alex Kositskiy <koso-96@mail.ru>
2023-04-19 19:55:18 +0300

    update brain_gcd_logic.py

--- brain_games/games/brain_gcd_logic.py
+++ brain_games/games/brain_gcd_logic.py
@@ -1,20 +1,5 @@
 from random import randint
-
-
-def alg_evklid(num1, num2):
-    '''
-    Implementation of the Euclid algorithm.
-    :param num1: int
-    :param num2: int
-    :return: int
-    '''
-    while num1 != 0 and num2 != 0:
-        if num1 > num2:
-            num1 = num1 % num2
-        else:
-            num2 = num2 % num1
-
-    return num1 + num2
+from math import gcd
 
 
 def brain_gcd():
@@ -26,10 +11,10 @@ def brain_gcd():
     num1 = randint(1, 1000)
     num2 = randint(1, 10)
 
+    answer_game = gcd(num1, num2)
+
     print(TEXT_QUEST)
     print(f'Question: {num1} {num2}')
     answer_user = int(input('Your answer: '))
-
-    answer_game = alg_evklid(num1, num2)
 
     return answer_game, answer_user
