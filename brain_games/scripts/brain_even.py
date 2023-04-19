81f0f754966f57cf77850c93f8d528a4a7ae0635
Alex Kositskiy <koso-96@mail.ru>
2023-04-19 20:27:12 +0300

    update brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -4,6 +4,7 @@
 from brain_games.scripts.brain_games import Welcome
 from brain_games.cli import welcome_user
 from brain_games.games.brain_game_logic import brain_game
+from brain_games.games.brain_game_logic import end_game
 from brain_games.games.brain_even_logic import brain_even
 
 
@@ -11,10 +12,9 @@ def main():
     Welcome()
     name = welcome_user()
 
-    if brain_game(brain_even):
-        print(f'Congratulations, {name}!')
-    else:
-        print(f"Let's try again, {name}!")
+    win_lose = brain_game(brain_even)
+
+    end_game(win_lose, name)
 
 
 if __name__ == '__main__':
