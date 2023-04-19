357233ee7c237031dbfd71b1e3bbf6dbee74cb53
Alex Kositskiy <koso-96@mail.ru>
2023-04-19 19:41:30 +0300

    update brain_even_logic.py

--- brain_games/games/brain_even_logic.py
+++ brain_games/games/brain_even_logic.py
@@ -17,16 +17,15 @@ def check_even(num):
 
 def brain_even():
     '''
-    It's a game. It asks if the number is even.
-    Waiting for a 'yes' or 'no' answer.
-    return: str
+    Generates an even number question.
+    Calculates the answer for an expression.
+    Returns the question and its own answer.
+    return: str, str
     '''
     TEXT_QUEST = 'Answer "yes" if the number is even, otherwise answer "no".'
     num_quest = randint(0, 1000)
     answer_game = check_even(num_quest)
 
-    print(TEXT_QUEST)
-    print(f'Question: {num_quest}')
-    answer_user = input('Your answer: ')
+    TEXT_QUEST += '\n' + f'Question: {num_quest}'
 
-    return answer_game, answer_user
+    return answer_game, TEXT_QUEST
