83a79d96af340b8fd8ed5e5de549e47372317df8
Alex Kositskiy <koso-96@mail.ru>
2023-04-24 21:17:19 +0300

    update brain_gcd_logic.py

--- brain_games/games/brain_gcd_logic.py
+++ brain_games/games/brain_gcd_logic.py
@@ -2,23 +2,24 @@ from random import randint
 from math import gcd
 
 
+GAME_CONDITIONS = 'Find the greatest common divisor of given numbers.'
+
 MIN_RANGE = 1
 MAX_RANGE1 = 1000
 MAX_RANGE2 = 10
 
 
-def brain_gcd():
+def make_game_work():
     '''
     Generates a question about the greatest common divisor.
     Computes its own answer. Returns the question and its own answer.
     :return: int, str
     '''
-    text_quest = 'Find the greatest common divisor of given numbers.'
     num1 = randint(MIN_RANGE, MAX_RANGE1)
     num2 = randint(MIN_RANGE, MAX_RANGE2)
 
     answer_game = gcd(num1, num2)
 
-    text_quest += '\n' + f'Question: {num1} {num2}'
+    text_quest = f'{num1} {num2}'
 
     return answer_game, text_quest
