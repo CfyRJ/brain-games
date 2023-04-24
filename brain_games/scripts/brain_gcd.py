ee6323390d2a5deeaa69b91be66406a3638b884a
Alex Kositskiy <koso-96@mail.ru>
2023-04-24 21:19:14 +0300

    update brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -3,16 +3,16 @@
 
 from brain_games.scripts.brain_games import welcome
 from brain_games.cli import ask_name_user
-from brain_games.games.brain_game_logic import brain_game
+from brain_games.games.brain_game_logic import start_game
 from brain_games.games.brain_game_logic import end_game
-from brain_games.games.brain_gcd_logic import brain_gcd
+from brain_games.games import brain_gcd_logic
 
 
 def main():
     welcome()
     name = ask_name_user()
 
-    win_lose = brain_game(brain_gcd)
+    win_lose = start_game(brain_gcd_logic)
 
     end_game(win_lose, name)
 
