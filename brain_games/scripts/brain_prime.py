de6615ee75c53803f466619056ef9c6e5fc83913
Alex Kositskiy <koso-96@mail.ru>
2023-04-24 21:19:26 +0300

    update brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -3,16 +3,16 @@
 
 from brain_games.scripts.brain_games import welcome
 from brain_games.cli import ask_name_user
-from brain_games.games.brain_game_logic import brain_game
+from brain_games.games.brain_game_logic import start_game
 from brain_games.games.brain_game_logic import end_game
-from brain_games.games.brain_prime_logic import brain_prime
+from brain_games.games import brain_prime_logic
 
 
 def main():
     welcome()
     name = ask_name_user()
 
-    win_lose = brain_game(brain_prime)
+    win_lose = start_game(brain_prime_logic)
 
     end_game(win_lose, name)
 
