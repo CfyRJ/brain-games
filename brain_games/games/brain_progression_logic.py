a4ab12d83cc6c72c06dd043cf79fc83cf835b9ce
Alex Kositskiy <koso-96@mail.ru>
2023-04-24 21:17:56 +0300

    update brain_progression_logic.py

--- brain_games/games/brain_progression_logic.py
+++ brain_games/games/brain_progression_logic.py
@@ -1,6 +1,8 @@
 from random import randint
 
 
+GAME_CONDITIONS = 'What number is missing in the progression?'
+
 MIN_COUNT = 5
 MAX_COUNT = 10
 MIN_RANGE = 1
@@ -9,14 +11,13 @@ MIN_STEP = 2
 MAX_STEP = 17
 
 
-def brain_progression():
+def make_game_work():
     '''
     Generates a question about a missing progression element.
     Calculates the answer for an expression.
     Returns the question and its own answer.
     :return: int, str
     '''
-    text_quest = 'What number is missing in the progression?'
     count_el = randint(MIN_COUNT, MAX_COUNT)
     first_el = randint(MIN_RANGE, MAX_RANGE)
     step = randint(MIN_STEP, MAX_STEP)
@@ -31,6 +32,6 @@ def brain_progression():
     progression = list(map(str, progression))
     progression = ' '.join(progression)
 
-    text_quest += '\n' + f'Question: {progression}'
+    text_quest = f'{progression}'
 
     return answer_game, text_quest
