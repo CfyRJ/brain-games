b96292c259db9b6bc2f6de1449f1457aed376a81
Alex Kositskiy <koso-96@mail.ru>
2023-04-24 20:01:14 +0300

    update brain_calc_logic.py

--- brain_games/games/brain_calc_logic.py
+++ brain_games/games/brain_calc_logic.py
@@ -2,9 +2,9 @@ import operator
 from random import randint, choice
 
 
-MIN_EL_RANGE = 0
-MAX_EL_FIRST_RANGE = 1000
-MAX_EL_SEC_RANGE = 10
+MIN_RANGE = 0
+MAX_RANGE1 = 1000
+MAX_RANGE2 = 10
 
 
 def calculate_expression(num1, num2, oper):
@@ -15,13 +15,13 @@ def calculate_expression(num1, num2, oper):
     :param oper: The function to be called.
     :return: str
     '''
-    operation_dict = {
+    operations = {
         "+": operator.add,
         "-": operator.sub,
         "*": operator.mul,
     }
 
-    return str(operation_dict[oper](num1, num2))
+    return str(operations[oper](num1, num2))
 
 
 def brain_calc():
@@ -32,11 +32,11 @@ def brain_calc():
     :return: str
     '''
     text_quest = 'What is the result of the expression?'
-    list_operations = ['+', '-', '*']
+    operations = ['+', '-', '*']
 
-    num1 = randint(MIN_EL_RANGE, MAX_EL_FIRST_RANGE)
-    num2 = randint(MIN_EL_RANGE, MAX_EL_SEC_RANGE)
-    operation = choice(list_operations)
+    num1 = randint(MIN_RANGE, MAX_RANGE1)
+    num2 = randint(MIN_RANGE, MAX_RANGE2)
+    operation = choice(operations)
 
     answer_game = calculate_expression(num1, num2, operation)
 
