4714233bf0ab992cd6cbaaaf94ba2f79532217dc
Alex Kositskiy <koso-96@mail.ru>
2023-04-24 21:16:50 +0300

    update brain_even_logic.py

--- brain_games/games/brain_even_logic.py
+++ brain_games/games/brain_even_logic.py
@@ -1,6 +1,8 @@
 from random import randint
 
 
+GAME_CONDITIONS = 'Answer "yes" if the number is even, otherwise answer "no".'
+
 MIN_RANGE = 0
 MAX_RANGE = 1000
 
@@ -15,17 +17,16 @@ def is_even(num):
     return num % 2 == 0
 
 
-def brain_even():
+def make_game_work():
     '''
     Generates an even number question.
     Calculates the answer for an expression.
     Returns the question and its own answer.
     return: str, str
     '''
-    text_quest = 'Answer "yes" if the number is even, otherwise answer "no".'
     num_quest = randint(MIN_RANGE, MAX_RANGE)
     answer_game = is_even(num_quest)
 
-    text_quest += '\n' + f'Question: {num_quest}'
+    text_quest = f'{num_quest}'
 
     return answer_game, text_quest
