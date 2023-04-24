18ecf759c692b0baffe0ef4a05686fa0c6331b41
Alex Kositskiy <koso-96@mail.ru>
2023-04-24 21:19:44 +0300

    update brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -3,16 +3,16 @@
 
 from brain_games.scripts.brain_games import welcome
 from brain_games.cli import ask_name_user
-from brain_games.games.brain_game_logic import brain_game
+from brain_games.games.brain_game_logic import start_game
 from brain_games.games.brain_game_logic import end_game
-from brain_games.games.brain_progression_logic import brain_progression
+from brain_games.games import brain_progression_logic
 
 
 def main():
     welcome()
     name = ask_name_user()
 
-    win_lose = brain_game(brain_progression)
+    win_lose = start_game(brain_progression_logic)
 
     end_game(win_lose, name)
 
