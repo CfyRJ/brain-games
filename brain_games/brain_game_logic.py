f2bc77f53ba1e0c53c2f1a4f417c1f7fab6911e8
Alex Kositskiy <koso-96@mail.ru>
2023-05-03 21:52:50 +0300

    update brain_game_logic.py

--- brain_games/brain_game_logic.py
+++ brain_games/brain_game_logic.py
@@ -1,67 +1,41 @@
+import prompt
+
+
 COUNT_REPEAT = 3
 
 
-def ask_quest(text_quest):
-    '''
-    Ask question. Return answer.
-    :param text_quest: str
-    :return: str
-    '''
-    print(f'Question: {text_quest}')
-    answer_user = input('Your answer: ')
+def play_game(module_game):
+    """
+    Play the game.
+    :param module_game: module
+    :return: None
+    """
 
-    return answer_user
+    print('Welcome to the Brain Games!')
 
-
-def is_answers(answer_game, answer_user):
-    '''
-    Checking the correctness of the answer.
-    :param answer_game: any
-    :param answer_user: any
-    :return: bool
-    '''
-
-    return True if answer_user == str(answer_game) else False
-
-
-def start_game(module_game):
-    '''
-    Starts the game.
-    :param game: module
-    :return: bool
-    '''
-
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
 
     for i in range(COUNT_REPEAT):
         answer_game, text_quest = module_game.make_game_data()
 
         print(module_game.GAME_CONDITION)
-        answer_user = ask_quest(text_quest)
+
+        print(f'Question: {text_quest}')
+        answer_user = input('Your answer: ')
 
         if type(answer_game) == bool:
             answer_game = 'yes' if answer_game else 'no'
 
-        if is_answers(answer_game, answer_user):
+        if answer_user == str(answer_game):
             print('Correct!')
             continue
         else:
             print(f"'{answer_user}' is wrong answer ;(.", end=' ')
             print(f"Correct answer was '{answer_game}'.")
-            return False
+            print(f"Let's try again, {name}!")
+            break
     else:
-        return True
-
-
-def end_game(win_lose, name):
-    '''
-    Says win or lose.
-    :param win_lose: bool
-    :param name: str
-    :return: None
-    '''
-    if win_lose:
         print(f'Congratulations, {name}!')
-    else:
-        print(f"Let's try again, {name}!")
 
     return None
