052ffca83edd151601b83d3337c839d610263511
Alex Kositskiy <koso-96@mail.ru>
2023-05-04 20:13:45 +0300

    bugFix brain_even_logic.py

--- brain_games/games/brain_even_logic.py
+++ brain_games/games/brain_even_logic.py
@@ -8,22 +8,22 @@ MAX_RANGE = 1000
 
 
 def is_even(num):
-    '''
+    """
     Checking a number for evenness.
     :param num: int
     :return: bool
-    '''
+    """
 
     return num % 2 == 0
 
 
 def make_game_data():
-    '''
+    """
     Generates an even number question.
     Calculates the answer for an expression.
     Returns the question and its own answer.
     return: str, str
-    '''
+    """
     num_quest = randint(MIN_RANGE, MAX_RANGE)
     answer_game = 'yes' if is_even(num_quest) else 'no'
 
