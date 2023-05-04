3c6772398d95d100bcb0a33b8eae9f0c3381bf58
Alex Kositskiy <koso-96@mail.ru>
2023-05-04 20:12:56 +0300

    bugFix cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,10 +2,10 @@ import prompt
 
 
 def ask_name_user():
-    '''
+    """
     Requests and returns the username.
     :return: str
-    '''
+    """
 
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
